163f0221bd7c720ebe54364874c288e12fe952a2
Alex villada <avillaberrio@gmail.com>
2024-03-18 15:31:41 -0500

    First commit to agg

---
+++ indice.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+numero_1 =   int(input("ingrese el numero 1: "))
+numero_2 =   int(input("ingrese el numero 2: "))
+
+resultado = numero_1 * numero_2
+
+raiz_cuadrada = np.sqrt(resultado)
+print(raiz_cuadrada)
